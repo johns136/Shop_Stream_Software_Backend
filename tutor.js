95ee0d0fd0d054563ae24950206c6b20cc4aaae5
Jared Johnson <johns136@augsburg.edu>
2019-11-23 11:02:51 -0600

    Update tutor.js

--- tutor.js
+++ tutor.js
@@ -1,5 +1,6 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
+//const Promise = require('promise');
 // Connection URL
 //const url = 'mongodb://localhost/EmployeeDB';
 const url = 'mongodb://localhost:27017/';
@@ -10,128 +11,156 @@ const client = MongoClient(url, { useUnifiedTopology: true });
 //const client = new MongoClient(url, { useNewUrlParser: true });
 
 // Use connect method to connect to the Server
-client.connect(async function(err, db) {
 
-  var ran1 = randomInt(1, 100);
-  var ran2 = randomInt(1,100);
-  var curdate = new Date();
-
-  var dbo = db.db("mydb");    //database name
-
-  var mycol = "Material Inventory";    //collection name
-  var customercol = "Customers";
-  var partcol = "Part Setup";
-  var jobcol = "Jobs";
-  var inspectcol = "Inspect";
-  var shiftcol = "Shift";
-  var jarray = [];//an array of job_nums
+  client.connect(async function(err, db) {
 
 
-  //var mymat = materialFormat(db,mycol);
-  var matobj = {
-    name: "Material "+ran1,
-    type: "Metal",
-    isMatieral: true,
-    isTool: false,
-    quantity: ran2,
-    length: .375,
-    width: 17.0,
-    height: 10.0,
-    jobs_for: "Job 1"
-  }; //obj editing\
+      var ran1 = randomInt(1, 100);
+      var ran2 = randomInt(1,100);
+      var curdate = new Date();
 
-   var customerobj = {
-     Buyer: "Jenkins",
-     Company: "Incorporated",
-     Email: "dude@gmail.com",
-     Phone: 763-333-3333
-   };
-  var inspectobj = {
-    date: new Date('2020-01-01'),
-    Due_Date: new Date('2020-01-05'),
-    Description: "good inspection",
-    Job_Number: "get job",
-    Quantity_to_Ship: ran2 ,
-    Materials: "good"
-    //getfromJob - partnum, jobnum, duedate
-    //getfromCustomer - company,buyer
-  };
-  var shiftobj = {
-    //getfromJob - partnum, jobnum,
-    Job_Num: 47,    //select a specific job_num that's available
-    button_to_button_time: '02:30',
-    Machine_Time: '01:15' ,
-    Parts_Sampled: ran1,
-    Date: new Date('2020-01-01'),
-    Notes: "Some notes"
-  };
-  var jobobj = {
-    OrderDate: new Date('2020-01-01'),
-    PO_Num: ran1,
-    Job_Num: ran1,
-    Part_Num: ran2,
-    Order_Qty: 30,
-    Recieve_Qty: 0,
-    //Remain_Qty: findQueryFunction(dbo,jobcol,Order_Qty) - findQueryFunction(dbo,jobcol,Recieve_Qty) ,
-    Cycle_Time: '02:40',
-    Run_Hours: 30,
-    Run_Days: 1,
-    Due_Date: new Date('2020-01-05') ,
-    MMENotes: "Notes",
-    Amount: {value: parseFloat("11.99"), currency: "USD"},
-    PerHour: {value: parseFloat("1.99"), currency: "USD"},
-    PerUnit: {value: parseFloat("5.99"), currency: "USD"},
-    Total: {value: parseFloat("11.99"), currency: "USD"}
+      var dbo = db.db("mydb");    //database name
 
-  };
-  var partobj = {
-    //get partnum from Jobs
-    button_to_button_time: "01:20",
-    Description: "good part",
-    Machine_Time: "02:20",
-    ToolNotes: "Good Tool",
-    ViceNotes: "vice",
-    Scraps: {isScraps: true, scrapQuanity: 10, Types: "silver, gold, copper"}
-    //on Front-End, try to submit it so it comes in with this format
-  };
+      var mycol = "Material Inventory";    //collection name
+      var customercol = "Customers";
+      var partcol = "Part Setup";
+      var jobcol = "Jobs";
+      var inspectcol = "Inspect";
+      var shiftcol = "Shift";
+      var jarray = [];//an array of job_nums
 
-  var newobj = {$set: {status: "broken", quantity: 11 }}; //update object
-  var query = { name: "Company Inc" };  //find query
-  var mysort = { quantity: -1 };         //sort type
-  //var a = db.jobcol.find().toArray();
-  await jarrayFunction(dbo, jobcol, jarray);
 
-  //deleteFunction(dbo,mycol, myobj);
-  //insertFunction(dbo,customercol, customerobj,jarray);
-  //insertFunction(dbo,mycol, matobj,jarray);
-  insertFunction(dbo,partcol,partobj,jarray);
-  //insertFunction(dbo,jobcol,jobobj,jarray);
+      //var mymat = materialFormat(db,mycol);
+      var matobj = {
+        name: "Material "+ran1,
+        type: "Metal",
+        isMatieral: true,
+        isTool: false,
+        quantity: ran2,
+        length: .375,
+        width: 17.0,
+        height: 10.0,
+        //jobs_for: "Job 1"
+      }; //obj editing\
+      var jpartnum = await findJob(dbo, 30, jarray);
+      console.log("jpartnum "+jpartnum);
+       var customerobj = {
+         Buyer: "Jenkins",
+         Company: "Incorporated",
+         Email: "dude@gmail.com",
+         Phone: 763-333-3333
+       };
+      var inspectobj = {
+        date: new Date('2020-01-01'),
+        Due_Date: new Date('2020-01-05'),
+        Description: "good inspection",
+        Job_Number: 30,
+        Quantity_to_Ship: ran2 ,
+        Materials: "good" ,
+        Part_Num: jpartnum
+        //getfromCustomer - company,buyer
+      };
+      var shiftobj = {
+        //getfromJob - partnum, jobnum,
+        Job_Num: 47,    //select a specific job_num that's available
+        Part_Num: await findJob(dbo, 47, jarray),
+        button_to_button_time: '02:30',
+        Machine_Time: '01:15' ,
+        Parts_Sampled: ran1,
+        Date: new Date('2020-01-01'),
+        Notes: "Some notes"
+      };
+      var jobobj = {
+        OrderDate: new Date('2020-01-01'),
+        PO_Num: ran1,
+        Job_Num: ran1,
+        Part_Num: ran2,
+        Order_Qty: 30,
+        Recieve_Qty: 0,
+        //Remain_Qty: findQueryFunction(dbo,jobcol,Order_Qty) - findQueryFunction(dbo,jobcol,Recieve_Qty) ,
+        Cycle_Time: '02:40',
+        Run_Hours: 30,
+        Run_Days: 1,
+        Due_Date: new Date('2020-01-05') ,
+        MMENotes: "Notes",
+        Amount: {value: parseFloat("11.99"), currency: "USD"},
+        PerHour: {value: parseFloat("1.99"), currency: "USD"},
+        PerUnit: {value: parseFloat("5.99"), currency: "USD"},
+        Total: {value: parseFloat("11.99"), currency: "USD"}
 
-  //insertFunction(dbo,inspectcol,inspectobj,jarray );
-  //insertFunction(dbo,shiftcol,shiftobj,jarray);
-  //insertFunction(dbo,mycol, mymat,jarray);
-  //updateFunction(dbo,mycol, myobj, newobj);
-  //findAllFunction(dbo,mycol, myobj);
-  //findQueryFunction(dbo,mycol, query);
-  //sortDatabase(dbo,mycol, mysort);
-  //deleteCollection(dbo, mycol);
-  //jarray.push(1);
+      };
+      var partobj = {
+        //get partnum from Jobs
+        Job_Num: 95,
+        Part_Num: await findJob(dbo, 95, jarray),
+        button_to_button_time: "01:20",
+        Description: "good part",
+        Machine_Time: "02:20",
+        ToolNotes: "Good Tool",
+        ViceNotes: "vice",
+        Scraps: {isScraps: true, scrapQuanity: 10, Types: "silver, gold, copper"}
+        //on Front-End, try to submit it so it comes in with this format
+      };
 
-  console.log(jarray);
-  client.close();
-});
+      var newobj = {$set: {status: "broken", quantity: 11 }}; //update object
+      var query = { name: "Company Inc" };  //find query
+      //var jq  =
+      var mysort = { quantity: -1 };         //sort type
+      //var a = db.jobcol.find().toArray();
+      await jarrayFunction(dbo, jobcol, jarray);
+
+      //deleteFunction(dbo,mycol, myobj);
+      //insertFunction(dbo,customercol, customerobj,jarray);
+      //insertFunction(dbo,mycol, matobj,jarray);
+      insertFunction(dbo,partcol,partobj,jarray);
+      //insertFunction(dbo,jobcol,jobobj,jarray);
+
+      //insertFunction(dbo,inspectcol,inspectobj,jarray );
+      insertFunction(dbo,shiftcol,shiftobj,jarray);
+      //insertFunction(dbo,mycol, mymat,jarray);
+      //updateFunction(dbo,mycol, myobj, newobj);
+      //findAllFunction(dbo,mycol, myobj);
+      //findQueryFunction(dbo,mycol, query);
+      //sortDatabase(dbo,mycol, mysort);
+      //deleteCollection(dbo, mycol);
+      //jarray.push(1);
+
+      //console.log(jarray);
+      //var jtest = await findJob(dbo, 30, jarray);
+      //console.log(jtest)
+      client.close();
+    });
+
+
 
 const jarrayFunction = async function(db, col, jarray){
-  var dbo = db;
+  //var dbo = db;
   var findjobs = db.collection(col).find({Job_Num: {$exists:true}});
   await findjobs.forEach(function(document){
 
       jarray.push(document.Job_Num);
-
-      //console.log(jarray);
+      //console.log("before jarray");
+      //console.log("jarray " +jarray);
   });
-
   console.log("jarray function complete");
+}
+
+const findJob = async function(db, jobnum, jarray){
+  a = null;
+  var start = db.collection('Jobs').find({Job_Num: jobnum});
+  await start.forEach(function(document){
+    //console.log(result);
+    //console.log("Part_Num = "+result[0].Part_Num);
+    a = document.Part_Num;
+    //console.log("before");
+    //console.log("a at this point " +a);
+    //a = specval(result[0].Part_Num);
+
+  });
+  console.log("findJobNum Function complete");
+  //console.log("returning " +a);
+  return a;
+
 }
 
 //insert myobj into collection customers
@@ -203,6 +232,13 @@ const deleteCollection = function(db, col){
   })
 }
 
+
+
+const specval = async function(val){
+  //val[0].Part_Num;
+  //console.log("specval function " +val);
+  return val;
+}
 
 const randomInt = function(low, high) {
   return Math.floor(Math.random() * (high - low + 1) + low)
